a646da1de542fa24c3c01e5f5cb0389af7fae441
unknown <you@example.com>
2019-03-18 14:07:00 +0100

    CRUD Questions

---
+++ resources/js/Router/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+import Login from '../components/login/Login'
+import Signup from '../components/login/Signup'
+import Logout from '../components/login/Logout'
+import Forum from '../components/forum/Forum'
+import Read from '../components/forum/Read'
+import Create from '../components/forum/create'
+
+const routes = [
+     { path: '/login', component: Login },
+     { path: '/logout', component: Logout },
+     { path: '/signup', component: Signup },
+     { path: '/ask', component: Create },
+     { path: '/forum', component: Forum ,name:'forum'},
+     { path: '/question/:slug', component: Read ,name:'read'},
+
+
+  ]
+
+  const router = new VueRouter({
+    routes, // short for `routes: routes`
+    hashbang : false,
+    mode : 'history'
+  })
+
+
+  export default router
